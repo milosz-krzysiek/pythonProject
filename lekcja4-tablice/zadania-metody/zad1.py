d4da63a7135e094b71fb72f172c08159697869b6
milosz-krzysiek <miloszkrzysiek@spgorno.edu.pl>
2024-02-24 14:00:08 +0100

    listy metody

--- lekcja4-tablice/zadania-metody/zad1.py
+++ lekcja4-tablice/zadania-metody/zad1.py
@@ -10,10 +10,49 @@ def wywołanieZadania1():
     rezulat = zadanie1(liszba=liczba)
     print(rezulat)
 
-def zadanie2():
-    pass
+
+def zadanie2(x, y):
+    lista = []
+    while(x < y):
+        if(x % 2 == 0):
+            lista.append(x)
+        x = x + 1
+    return lista
 
 def wywołanieZadania2():
-    pass
+    liczba1 = int(input("Podaj pierwszą liczbę "))
+    liczba2 = int(input("Podaj drugą liczbę "))
+    rezulat = zadanie2(x = liczba1, y = liczba2)
+    print(rezulat)
 
 #wywołanieZadania1()
+#wywołanieZadania2()
+
+def zadanie3(x, y):
+    lista = []
+    while(x < y):
+        if(x % 2 != 0):
+            lista.append(x)
+        x = x + 1
+    lista.reverse()
+    return lista
+
+def wywołanieZadania3():
+    liczba1 = int(input("Podaj pierwszą liczbę "))
+    liczba2 = int(input("Podaj drugą liczbę "))
+    rezulat = zadanie3(x = liczba1, y = liczba2)
+    print(rezulat)
+
+#wywołanieZadania3()
+    
+def zadanie4():
+    pass
+
+def wywołanieZadania4():
+    pass
+
+def zadanie5():
+    pass
+
+def wywołanieZadania5():
+    pass
