2e274d3bfddba320ef0daca6442f7986f9b4365d
milosz-krzysiek <miloszkrzysiek@spgorno.edu.pl>
2024-02-25 21:59:32 +0100

    bryh

---
+++ lekcja9-ciągi_znaków/wprowadzenie.py
@@ -0,0 +1,37 @@
+'''
+x = 'Technik'
+y = 'Informatyk'
+z = x + y
+print(z) 
+'''
+'''
+x = 'Technik'
+y = 'Informatyk'
+z = 4*x
+print(z)
+'''
+'''
+x = 25
+y = '25'
+print(type(x))
+print(type(y))
+
+Typ zmiennej x: int
+Typ zmiennej y: str
+'''
+
+#x = '73271'
+#y = 723182
+#print(x, y)
+
+#x = 25
+#y = '25'
+#z = x + int(y)
+#print(z)
+
+#x = 25
+#y = '25'
+#z = str(x) + y
+#print(z)
+x = "technik"
+print (x[-6:len(x):2])
