94a5cfdccbd599e5fb05924e821378be484d431e
milosz-krzysiek <miloszkrzysiek@spgorno.edu.pl>
2024-03-06 16:44:45 +0100

    kalendarz - poczatkowe wersje + funkcje do zaokraglania

---
+++ kalkulator/1.1.py
@@ -0,0 +1,29 @@
+liczba_1 = 0
+liczba_2 = 0
+znak = ''
+
+rownanie = input("Podaj równanie do rozwiązania, oddzielaj wszystko spacją:\n")
+rownanie = rownanie.split()
+
+liczba_1 = float(rownanie[0])
+znak = rownanie[1]
+liczba_2 = float(rownanie[2])
+
+match znak:
+    case '+':
+        wynik = liczba_1 + liczba_2
+    case '-':
+        wynik = liczba_1 - liczba_2
+    case '*':
+        wynik = liczba_1 * liczba_2
+    case '/':
+        if(liczba_2 == 0):
+            print("Nie dziel przez '0'")
+            exit()
+        else:
+            wynik = liczba_1 / liczba_2
+    case _:
+        print("Błędny znak")
+        exit()
+
+print(wynik)
